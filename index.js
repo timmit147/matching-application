85008fa8ef427316c46e88a835e16b8c055a694f
root <root@DESKTOP-DMFD3QJ.localdomain>
2021-02-20 16:53:36 +0100

    form get

--- index.js
+++ index.js
@@ -2,49 +2,70 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var path = require('path');
-
-
+const path = require('path');
 
 // Pug
 const pug = require('pug');
 
-// save variable in txt
-const fs = require('fs');
+app.set('view engine', 'pug')
 
-var a = 0;
 
+const movies = [
+  { "id": "black-panther", "name": "Black Panther", "year": 2018, "categories": ["action","adventure","sci-fi"],"storyline": "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past."},
+  { "id": "incredibles-3", "name": "Incredibles 2", "year": 2018, "categories": ["animation","action","adventure"],"storyline": "While the Parr family has accepted its collective calling as superheroes, the fact remains that their special heroism is still illegal. After they are arrested after unsuccessfully trying to stop the Underminer, their future seems bleak. However, the wealthy Deavor siblings of Devtech offer new hope with a bold project to rehabilitate the public image and legal status of Supers, with Elastigirl being assigned on point to be the shining example. Now having agreed for now to stay at home to care of the kids, Mr. Incredible finds domestic life a daunting challenge, especially with baby Jack-Jack's newly emerged powers making him almost impossible to manage. However, Elastigirl soon has her own concerns dealing with the menace of a new supervillain, Screenslaver, who is wreaking havoc with his mind control abilities. Now, Elastigirl must solve the mystery of this enemy, who has malevolent designs on the world with the Parr family and friends key targets of this evil. Written by Kenneth Chisholm (kchishol@rogers.com)"},
+  { "id": "halloween", "name": "Halloween", "year": 2018, "categories": ["horror","thriller"],"storyline": "Laurie Strode comes to her final confrontation with Michael Myers, the masked figure who has haunted her since she narrowly escaped his killing spree on Halloween night four decades ago."},
+  { "id": "ad-astra", "name" : "Ad Astra", "year" : 2019, "categories" : ["adventure","fantasy","mystery","thriller","sci-fi"], "storyline" : "Thirty years ago, Clifford McBride led a voyage into deep space, but the ship and crew were never heard from again. Now his son -- a fearless astronaut -- must embark on a daring mission to Neptune to uncover the truth about his missing father and a mysterious power surge that threatens the stability of the universe."},
+  { "id": "toy-story-4", "name" : "Toy Story 4", "year" : 2019, "categories" : ["animation","adventure","comedy","family","fantasy"],  "storyline" : "When a new toy called Forky joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy."}
+];
 
 app.get('/', (req, res) => {
 
-	// localhost open index
-	res.sendFile(path.join(__dirname + '/public/index.html'));
+	  res.render('profiel', { 
+  	title: 'Films', 
+  	message: 'Zoek je favorieten films om een match te maken.',
+  	movies
+  })
 
 
-	// Use all files in public
- 	app.use(express.static('public'));
+});
 
- 	// Funtion when 404 
-	app.use(function(req, res, next){
-		res.status(404).send('Page not found');
-	});
+app.get('/bedankt', (req, res) => {
+var name = req.query.name;
+    res.render('bedankt', { 
+      title: name, 
+  })
+
 
 });
 
 
 
+app.get('/changeinfo', (req, res) => {
+
+	  res.render('changeinfo', { 
+  	title: 'Films', 
+  	message: 'Zoek je favorieten films om een match te maken.',
+  	movies
+  })
+
+
+});
+
+
+
+
+
 app.listen(port, () => {
 
-	  	// read file
-	fs.readFile(__dirname + '/public/tekst.txt', 'utf8', function (err,data) {
-	  if (err) {
-	    a = 99;
-	  }
-	  a = data;
-	});
 
   console.log(`Example app listening at http://localhost:${port}`);
 
+	// Use all files in public
+ 	app.use(express.static('public'));
+
+ 		app.use(function(req, res, next){
+		res.status(404).send('Page not found');
+	});
 
 
 });
@@ -52,28 +73,7 @@ app.listen(port, () => {
 
 
 // waneer je naar /pizza gaat gaat die een template laden
-app.get('/pizza', (req, res) => {
+app.get('/test', function (req, res) {
+	
 
-
-// laat een template zien met variabel nummer
-  res.send(pug.renderFile('public/template.pug', {
-  		name: a
-	}
-	));
-
-
-
-a = parseInt(a)+ 1;
-
-fs.writeFile(__dirname + '/public/tekst.txt', a.toString(), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!" + a.toString());
-}); 
-
-
-
-
-
-});
+})
--- test.js
+++
@@ -1,1 +0,0 @@
-console.log("hallo dit is een test")
